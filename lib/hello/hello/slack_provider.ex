728f877f1e6b86a1124bdac8c13a0adf7ec90115
TORIFUKUKaiou <torifuku.kaiou@gmail.com>
2019-10-13 00:09:12 +0900

    Refactor Hello.SlackProvider
    Remove 'list' variable.
    Apply pattern match to the function of replaced_channels/2.

--- lib/hello/hello/slack_provider.ex
+++ lib/hello/hello/slack_provider.ex
@@ -10,43 +10,30 @@ defmodule Hello.SlackProvider do
   def user_list do
     channels = channel_list
 
-    list =
-      Slack.Web.Users.list(%{token: api_token})
-      |> Map.fetch!("members")
-      |> Enum.map(fn u -> replace_channel_info(u, channels) end)
-
-    list
+    Slack.Web.Users.list(%{token: api_token})
+    |> Map.fetch!("members")
+    |> Enum.map(fn u -> replace_channel_info(u, channels) end)
   end
 
   def channel_list do
-    list =
-      Slack.Web.Channels.list(%{token: api_token})
-      |> Map.fetch!("channels")
-      |> Enum.map(fn c -> {c["id"], c} end)
-      |> Map.new()
-
-    list
+    Slack.Web.Channels.list(%{token: api_token})
+    |> Map.fetch!("channels")
+    |> Enum.map(fn c -> {c["id"], c} end)
+    |> Map.new()
   end
 
   def replace_channel_info(user, channels) do
     Map.put(user, :guest_channels, replaced_channels(user["profile"]["guest_channels"], channels))
   end
 
+  def replaced_channels(nil, _), do: nil
+
+  def replaced_channels("[]", _), do: nil
+
+  def replaced_channels("", _), do: nil
+
   def replaced_channels(gclist, channels) do
-    list =
-      case gclist do
-        nil ->
-          nil
-
-        "[]" ->
-          nil
-
-        "" ->
-          nil
-
-        clist ->
-          Jason.decode!(clist)
-          |> Enum.map(fn c -> Map.get(channels, c) end)
-      end
+    Jason.decode!(gclist)
+    |> Enum.map(fn c -> Map.get(channels, c) end)
   end
 end
